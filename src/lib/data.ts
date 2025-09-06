387197d070c8a8429d051a908ca3d0f0123260bb
Bunny Preetham <bunnypreetham26@gmail.com>
2025-09-06 06:29:47 +0000

    some items such as Classic Sci-Fi Novel Collection do not have add to ca

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -39,7 +39,7 @@ export const mockProducts: Product[] = [
     category: 'Books',
     subcategory: 'Fiction',
     imageUrl: 'https://picsum.photos/600/400',
-    userId: '1',
+    userId: '3',
     imageHint: 'book collection'
   },
   {
